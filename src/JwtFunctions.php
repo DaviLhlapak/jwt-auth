cde5ea9f4f6b3a9cd6f041b5734ba40e2017463b
DaviLhlapakRosa <alpkauniverse@gmail.com>
2020-04-28 23:21:19 -0300

    Updated JwtFunctions

--- src/JwtFunctions.php
+++ src/JwtFunctions.php
@@ -4,25 +4,32 @@
 namespace PakPak\JwtAuth;
 
 
+use DateTime;
+
 class JwtFunctions{
 
     /**
+     * @param string $algo
      * @return array|string[]
      */
-    public static function createHeader():array{
+    public static function createHeader(string $algo):array{
         return [
             'typ' => 'JWT',
-            'alg' => 'HS256'
+            'alg' => $algo
         ];
     }
 
     /**
-     * @param string $subject
+     * @param string $issuer Token's origin
+     * @param string $subject Entity to which the token belongs
+     * @param int $expiration Timestamp of when the token expires
      * @return array|string[]
      */
-    public static function createPayload(string $subject):array{
+    public static function createPayload(string $issuer, string $subject, int $expiration):array{
         return [
-            'iss' => "localhost",
+            'iss' => $issuer,
+            'iat' => (new DateTime("now"))->getTimestamp(),
+            'exp' => $expiration,
             'sub' => $subject
         ];
     }
