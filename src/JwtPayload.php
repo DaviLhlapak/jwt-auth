d63e47d3be9cc094cd78d8c3530e0b16b10c2154
DaviLhlapakRosa <alpkauniverse@gmail.com>
2020-05-30 23:37:29 -0300

    Added JwtPayload class 📃

---
+++ src/JwtPayload.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace PakPak\JwtAuth;
+
+use DateTime;
+
+class JwtPayload
+{
+    /** Identifies the principal that issued the JWT. @var String */
+    private $issuer;
+
+    /** Identifies the principal that is the subject of the JWT. @var String */
+    private $subject;
+
+    /** Identifies the expiration time on or after which the JWT MUST NOT be accepted for processing. @var int */
+    private $expirationTime;
+
+    /** Identifies the time at which the JWT was issued. @var int */
+    private $issuedAt;
+
+    /**
+     * JwtPayload constructor.
+     * If $ expirationTime or $ issueAt is not provided, by default, the Payload will contain the expiration date of 1 day.
+     *
+     * @param String $issuer
+     * @param String $subject
+     * @param int $expirationTime
+     * @param int $issuedAt
+     */
+    public function __construct(string $issuer, string $subject, int $expirationTime = 0, int $issuedAt = 0){
+
+        if (empty($issuedAt)){
+            $this->issuedAt = (new DateTime("now"))->getTimestamp();
+        }else{
+            $this->issuedAt = $issuedAt;
+        }
+
+        if (empty($issuedAt)){
+            $this->expirationTime = (new DateTime("now"))->add(\DateInterval::createFromDateString("1 day"))->getTimestamp();
+        }else{
+            $this->expirationTime = $expirationTime;
+        }
+
+        $this->issuer = $issuer;
+        $this->subject = $subject;
+    }
+
+    public function getPayload():array{
+        return [
+            'iss' => $this->issuer,
+            'iat' => $this->issuedAt,
+            'exp' => $this->expirationTime,
+            'sub' => $this->subject
+        ];
+    }
+
+}
